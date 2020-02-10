d01af466e2ce9d7651c1aff2859e88cca0b4ca1a
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-02-10 15:50:48 -0800

    axios call returns user data

--- index.js
+++ index.js
@@ -27,9 +27,65 @@ const questions = [
 
 inquirer.prompt(questions)
     .then(function( {username} ) {
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-        
-        axios.get (queryUrl).then(function(response) {
-            console.log(response);
-        })
+         const queryUrl = `https://api.github.com/users/${username}`
+       
+         return axios.get (queryUrl).then(function(response) {
+            const data = response.data;
+            const user = {
+                "profile-img": data.avatar_url,
+                "user-name": data.login,
+                "location": data.location,
+                "github": data.html_url,
+                "blog": data.blog,
+                "bio": data.bio,
+                "repos": data.public_repos,
+                "followers": data.followers,
+                "following": data.following
+            };
+             console.log(user);
+        });
+       
     })
+    .catch(function(err) {
+        console.log(err);
+    });
+    // .then(function( {username} ) {
+    //     const querySearch = `followers`
+    //     const queryUrl = `https://api.github.com/users/${username}/${querySearch}`;
+
+    //      axios.get (queryUrl).then(function(response) {
+    //         const followerNames = response.data.map(function (followers) {
+    //             return followers.login
+    //         });
+    //         console.log(followerNames.length);
+    //     })
+    // })
+    // .then(function( {username} ){
+    //     const queryUrl = `https://api.github.com/users/${username}/following`;
+
+    //     axios.get (queryUrl).then(function(response) {
+    //         const followingUsers = response.data.map(function (following) {
+    //             return following.login;
+    //         });
+    //         console.log(followingUsers.length);
+    //     })
+    // })
+    // .then(function( {username} ){
+    //     const queryUrl = `https://api.github.com/users/${username}/starred`;
+
+    //     axios.get (queryUrl).then(function(response) {
+    //         const stargazerAmount = response.data.map(function (stargazer) {
+    //             // const add = (a, b) =>
+    //             // a + b;
+    //             // const starSum = stargazerAmount.reduce(add);
+    //             return stargazer.stargazer_count;
+    //         });
+    //         console.log(stargazerAmount);
+    //     })
+    // })
+  
+    
+    
+
+
+    
